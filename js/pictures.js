4d92b5a85211b280fa35468db2bcf5ee4cb1c097
Dobrynia <movist00@gmail.com>
2018-11-22 22:37:23 +0700

    Все плохо

--- js/pictures.js
+++ js/pictures.js
@@ -1,51 +1,54 @@
 'use strict';
 
-var DESCRIPTION_ITEM = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...',
+var DESCRIPTIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...',
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 
-var COMMENTS_ITEM = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
+var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-for (var i = 1; i <= 25; i++) { };
+var CONST = 25;
 
-function getRandomNumbers(min, max) {
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var picturesBlock = document.querySelector('.pictures');
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+
+for (var i = 1; i <= CONST; i++) {}
+
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/*for (var j = 1; j <= getRandomNumbers(1, 10); j++) {
-    if (j > 5) {
-      j = 1                                  Вот тут мой мозг сказал пока:)
-    }
-};*/
-
-function getRandomСomments() {
-  if (Math.random() >= 0.5) {
-    COMMENTS_ITEM[descriptionNumber]
-  }
-  COMMENTS_ITEM[descriptionNumber] + COMMENTS_ITEM[descriptionNumber]
+function getRandomElement(element) {
+  return element[Math.floor(Math.random() * element.length)];
 }
 
-var descriptionNumber = getRandomNumbers(0, 5);
-
-var photos = [
-  {
-    url: 'photos/' + i + '.jpg',
-    likes: getRandomNumbers(15, 200),
-    /*comments: ,*/
-    description: DESCRIPTION_ITEM[descriptionNumber]
-  },
-  {
-
+function getRandomСomment() {
+  var comment = getRandomElement(COMMENTS);
+  if (Math.random() >= 0.5) {
+    comment += ' ' + getRandomElement(COMMENTS);
   }
-];
+  comment += (' ' + getRandomElement(COMMENTS)) * 2;
+  return comment;
+}
 
-  //Math.round - округление до ближайшего целого
-  //Math.random() - случайные числа
+function getСomments() {
+  for (var i = 0; i < getRandomNumber(1, 10); i++) {
+    COMMENTS[i] = getRandomСomment();
+  }
+  return COMMENTS;
+}
 
-  //comments, массив строк — список комментариев, оставленных другими пользователями к этой фотографии.
-  //Количество комментариев к каждой фотографии произвольное(10, отображается только 5).
-  //Комментарии генерируются случайным образом. сделал getRandomСomments
-  /*Для формирования комментария необходимо взять одно или два случайных предложений ------- вроде сделал getRandomСomments
+function generatePhotos() {
+  for (var i = 0; i < CONST; i++) {
+    photos[i] = {
+      url: 'photos/' + (i + 1) + '.jpg',
+      likes: getRandomNumber(15, 200),
+      comments: getRandomСomment(),
+      description: getRandomElement(DESCRIPTIONS)
+    };
+  }
+}
